fc0d995535faeefd9fd9a892977f7f51226cca65
gmarsanich <giacomomarsanich@gmail.com>
2022-11-07 15:52:10 +0100

    Implemented random sample function Removed hardcoded inital dir value Moved vertical border further right Added function descriptions and other documentation

--- brickbreaker.hpp
+++ brickbreaker.hpp
@@ -12,6 +12,8 @@
 #include <array>
 #include <iostream>
 
+//////// CONSTANTS AND DATA STRUCTURES
+
 // Screen size constants
 #define SCREEN_WIDTH 52
 #define SCREEN_HEIGHT 20
@@ -54,12 +56,24 @@ std::array<std::array<int, 2>, NO_BRICKS> bricks = {{{2, 7},
 std::array<int, NO_BRICKS> visibleBricks = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 std::array<int, 2> sliderPos = {18, 22};
 std::array<int, 2> ballPos = {17, 26};
+std::array<int, 4> dirs = {1, 2, 3, 4};
+std::array<int, 4> starting_dirs = {1, 2};
 
 int startBall = 0;
-int dir = 1;  // 1-top right, 2-top left, 3-bottom left, 4-bottom right
+int dir;  // 1-top right, 2-top left, 3-bottom left, 4-bottom right
 int bricksLeft = NO_BRICKS;
 bool win = 0;
 bool lose = 0;
+
+//////// FUNCTIONS FOR MAIN FILE
+
+// Takes a random element from an std::array of any size
+template <std::size_t SIZE>
+int sample(std::array<int, SIZE> arr) {
+    srand(time(NULL));
+    int n = rand() % arr.size();
+    return n;
+}
 
 // Moves the cursor to an (x, y) position
 void gotoxy(int x, int y) {
@@ -92,11 +106,12 @@ void drawBorder() {
     for (int i = 0; i < SCREEN_HEIGHT; i++) {
         gotoxy(0, i);
         std::cout << "|";
-        gotoxy(SCREEN_WIDTH, i);
+        gotoxy(SCREEN_WIDTH + 2, i);
         std::cout << "|";
     }
 }
 
+// Draws the bricks
 void drawBricks() {
     for (int i = 0; i < NO_BRICKS; i++) {
         if (visibleBricks[i] == 1) {
@@ -106,6 +121,7 @@ void drawBricks() {
     }
 }
 
+// Checks if the ball hits the slider and changes the ball's direction if it does
 void ballHitSlider() {
     if (ballPos[1] >= sliderPos[1] && ballPos[1] <= sliderPos[1] + 8) {
         if (ballPos[0] == sliderPos[0] - 1) {
@@ -117,6 +133,7 @@ void ballHitSlider() {
     }
 }
 
+// Removes one brick if it's hit by the ball
 void ballHitBrick() {
     for (int i = 0; i < NO_BRICKS; i++) {
         if (visibleBricks[i] == 1) {
