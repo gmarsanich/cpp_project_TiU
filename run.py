5b688dfe4e9d15028788134c47414ca9a2ad31d8
gmarsanich <giacomomarsanich@gmail.com>
2022-11-15 15:31:12 +0100

    Added process status

--- run.py
+++ run.py
@@ -3,5 +3,7 @@ import os
 cmp = r"g++ -O2 .\brickbreaker.cpp -o .\brickbreaker.exe"
 run = r".\brickbreaker.exe"
 
+print("Compiling...")
 os.system(cmp)
+print("Done! Running executable...")
 os.system(run)
