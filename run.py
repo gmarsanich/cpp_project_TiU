a43e6d50dc28d1444ac292657259e87255a541d1
gmarsanich <giacomomarsanich@gmail.com>
2022-11-20 16:33:58 +0100

    Clears screen before running compilation

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import os
 cmp = r"g++ -O2 .\brickbreaker.cpp -o .\brickbreaker.exe"
 run = r".\brickbreaker.exe"
 
+os.system("clear")
 print("Compiling...")
 os.system(cmp)
 print("Done! Running executable...")
