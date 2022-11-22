f9fe5b23c01f93855291f19c4ce93fa3f3ce7d5d
gmarsanich <giacomomarsanich@gmail.com>
2022-11-22 12:13:52 +0100

    Replaced manual movement inputs with agent method

--- brickbreaker.cpp
+++ brickbreaker.cpp
@@ -1,89 +1,64 @@
 #include "brickbreaker.hpp"
 
+#include "agent.hpp"
+
 // Game loop function
 void play() {
     dir = getRandomInt(1, 2);  // Takes random direction
+    Player agent = agent;      // Initializing player
 
     while (1) {
+        dbg(true);
         system("clear");
         drawBricks();
-
-        gotoxy(0, 0);
-
-        std::cout << lives << " Lives left\n";
-        std::cout << "starting ball direction: " << dir << "\n";
-        std::cout << "Score: " << score;
 
         drawPaddle();
         drawBall();
 
-        // Checks for keyboard inputs - will be deprecated when the AI agent is implemented
-        if (kbhit()) {
-            char ch = getch();
-            if (ch == 'd' || ch == 'D' || ch == 77) {
-                if (paddle->getxPos() < 44)
-                    paddle->updatexPos(2);
-            }
-            if (ch == 'a' || ch == 'A' || ch == 75) {
-                if (paddle->getxPos() > 2)
-                    paddle->updatexPos(-2);
-            }
-            if (ch == 32) {
-                startBall = true;
-            }
-            if (ch == 27) {
-                break;
-            }
-        }
-
-        if (startBall == true) {
-            if (ball->getyPos() < paddle->getyPos()) {
-                lives -= 1;
-            }
+        if (agent.startGame()) {
+            agent.control(dir);
 
             if (dir == 1) {  // TOP RIGHT
+                ball->updatexPos(-1);
                 ball->updateyPos(2);
-                ball->updatexPos(1);
                 if (ball->getxPos() > MAX_X) {
                     dir = 2;
                 } else if (ball->getyPos() < MIN_Y) {
                     dir = 4;
                 }
             } else if (dir == 2) {  // TOP LEFT
-                ball->updateyPos(-2);
                 ball->updatexPos(-1);
+                ball->updateyPos(-2);
                 if (ball->getyPos() < MIN_Y) {
                     dir = 3;
                 } else if (ball->getxPos() < MIN_X) {
                     dir = 1;
                 }
             } else if (dir == 3) {  // BOTTOM LEFT
-                ball->updateyPos(2);
                 ball->updatexPos(1);
+                ball->updateyPos(2);
 
                 if (ball->getyPos() > MAX_Y) {
                     lives -= 1;
-                    drawPaddle();
-                    drawBall();
+                    resetPaddle();
+                    resetBall();
                 } else if (ball->getxPos() < MIN_X) {
                     dir = 4;
                 }
             } else if (dir == 4) {  // BOTTOM RIGHT
-                ball->updateyPos(2);
                 ball->updatexPos(1);
+                ball->updateyPos(2);
                 if (ball->getxPos() > MAX_X) {
                     dir = 3;
-                } else if (ball->getyPos() < MAX_Y) {
-                    drawPaddle();
-                    drawBall();
+                } else if (ball->getyPos() < paddle->getyPos()) {
                     lives -= 1;
                     resetBall();
                     resetPaddle();
                 }
             }
+            ballHitSlider();
         }
 
-        ballHitSlider();
         ballHitBrick();
 
         if (lives < 0) {
@@ -96,7 +71,7 @@ void play() {
             break;
         }
 
-        Sleep(30);
+        Sleep(10);
     }
 
     if (lose == true) {
@@ -105,12 +80,13 @@ void play() {
         gotoxy(10, 5);
         std::cout << " --------------------- ";
         gotoxy(10, 6);
-        std::cout << " |     YOU LOSE      | ";
+        std::cout << " |     GAME OVER      | ";
         gotoxy(10, 7);
         std::cout << " --------------------- ";
 
         gotoxy(10, 9);
-        std::cout << "Press any key to go back to the main menu.";
+        std::cout << "Press any key to exit the game.\n";
+        std::cout << "\tFinal score: " << score;
         getch();
     }
 
@@ -124,7 +100,9 @@ void play() {
         std::cout << " --------------------- ";
 
         gotoxy(10, 9);
-        std::cout << "Press any key to go back to the main menu.";
+        std::cout << "Press any key to exit the game.\n";
+        gotoxy(11, 10);
+        std::cout << "\tFinal score: " << score;
         getch();
     }
 }
@@ -150,4 +128,7 @@ int main() {
 
     std::cout << std::endl
               << std::endl;
+
+    system("clear");
+    exit(0);
 }
